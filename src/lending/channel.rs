da966b590bd41af0857dd15d6f4f2a02b972b80d
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-04-12 09:03:40 -0300

    Create channel.rs

---
+++ src/lending/channel.rs
@@ -0,0 +1,23 @@
+use lightning::chain::chaininterface::{BroadcasterInterface, FeeEstimator};
+use lightning::ln::channelmanager::{ChannelManager, ChannelManagerReadArgs};
+use lightning::ln::peer_handler::{MessageHandler, PeerHandler, SocketDescriptor};
+use lightning::util::config::UserConfig;
+use std::sync::Arc;
+
+let config = UserConfig::default(); // Set your Lightning node configuration here
+let channel_manager = ChannelManager::new(
+    chain_source, // Your chain interface implementation
+    keys_manager, // Your keys manager implementation
+    fee_estimator, // Your fee estimator implementation
+    tx_broadcaster, // Your broadcaster interface implementation
+    logger, // Your logging implementation
+    config,
+    Some(ChannelManagerReadArgs {
+        keys_manager_arc: Arc::new(keys_manager.clone()),
+        fee_estimator_arc: Arc::new(fee_estimator.clone()),
+    }),
+    message_handler, // Your message handler implementation
+    Some(socket_descriptor), // Your socket descriptor implementation
+);
+let peer_handler = PeerHandler::new(channel_manager);
+peer_handler.connect_to_peer(peer_address); // Connect to your counterparty's Lightning node

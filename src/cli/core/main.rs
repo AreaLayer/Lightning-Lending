07c13461f785dac0530798b3c6e72edb00d980d9
22388o⚡️ <83122757+22388o@users.noreply.github.com>
2023-08-31 08:07:25 -0300

    Update main.rs

--- src/cli/core/main.rs
+++ src/cli/core/main.rs
@@ -1,3 +1,6 @@
 use std::nostr::{Event, Relay, Keys};
 use std::lightning::{Balance, Channel, Invoice};
 
+use nostr::{Event, Npub, Relay, Event};
+use lightning::{Balace, Channel, HashPayment};
+use lightning::{Open channel, Close channel};

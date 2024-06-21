c7f762e83c84c368fedae94363f3222167471364
223880 <rafael.segala45@gmail.com>
2024-06-21 07:34:02 -0300

    Add fee for RBF and UTXOs

--- src/lending/fee.rs
+++ src/lending/fee.rs
@@ -1,4 +1,58 @@
-use crate::RBF;
+use bitcoin::BumpFee:::RBF;
 
-impl RBF = {
+fn main() {
+    let mut rbf = RBF::new();
+}
+
+fn bump_fee() {
+    let mut rbf = RBF::new();
+    rbf.bump_fee();
+}
+
+fn bump_fee_with_conf() {
+    let mut rbf = RBF::new();
+    rbf.bump_fee_with_conf();
+}
+fn bump_fee_with_conf_and_utxo() {
+    let mut rbf = RBF::new();
+    rbf.bump_fee_with_conf_and_utxo();
+}
+fn bump_fee_with_conf_and_utxo_and_utxo_provider() {
+    let mut rbf = RBF::new();
+    rbf.bump_fee_with_conf_and_utxo_and_utxo_provider();
+}
+
+fn get_utxo_provider() {
+    let mut rbf = RBF::new();
+    rbf.get_utxo_provider();
+}
+
+fn set_utxo_provider() {
+    let mut rbf = RBF::new();
+    rbf.set_utxo_provider();
+}
+
+fn set_utxo_provider_with_conf() {
+    let mut rbf = RBF::new();
+    rbf.set_utxo_provider_with_conf();
+}
+
+fn set_utxo_provider_with_conf_and_utxo() {
+    let mut rbf = RBF::new();
+    rbf.set_utxo_provider_with_conf_and_utxo();
+}
+
+fn set_utxo_provider_with_conf_and_utxo_and_utxo_provider() {
+    let mut rbf = RBF::new();
+    rbf.set_utxo_provider_with_conf_and_utxo_and_utxo_provider();
+}
+impl RBF {
+    pub fn new() -> Self {
+        RBF
+    }
+}
+impl bump_fee { 
+    pub fn new() -> Self {
+        bump_fee
+    }
 }

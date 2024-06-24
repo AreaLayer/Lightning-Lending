44748bff0806d56eb8ba2e8a31c6965748bb7c29
223880 <rafael.segala45@gmail.com>
2024-06-24 07:27:01 -0300

    Fix

--- src/lending/nip_88.rs
+++ src/lending/nip_88.rs
@@ -1,1 +1,59 @@
+use crate::models::Nip;
+use nostr::PubKey;
+use nostr::Sign;
+use nostr::Event;
+use nostr::Relay;
 
+impl Nip {
+    pub fn new(pubkey: PubKey) -> Self {
+        Self {
+            pubkey,
+        }
+    }
+}
+
+impl Sign for Nip {
+    fn sign(&self, msg: &[u8]) -> Vec<u8> {
+        self.pubkey.sign(msg)
+    }
+}
+
+impl From<PubKey> for Nip {
+    fn from(pubkey: PubKey) -> Self {
+        Self {
+            pubkey,
+        }
+    }
+}
+
+impl From<&PubKey> for Nip {
+    fn from(pubkey: &PubKey) -> Self {
+        Self {
+            pubkey: pubkey.clone(),
+        }
+    }
+}
+
+impl From<Nip> for PubKey {
+    fn from(nip: Nip) -> Self {
+        nip.pubkey
+    }
+}
+
+impl From<&Nip> for PubKey {
+    fn from(nip: &Nip) -> Self {
+        nip.pubkey.clone()
+    }
+}
+
+impl From<Nip> for Event {
+    fn from(nip: Nip) -> Self {
+        Event::Nip(nip)
+    }
+}
+
+impl From<Relay> for Event {
+    fn from(relay: Relay) -> Self {
+        Event::Relay(relay)
+    }
+}

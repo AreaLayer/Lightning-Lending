4c45906a8ef4035b5c37d321317c8dbe2ab60fcb
223880 <rafael.segala45@gmail.com>
2024-06-29 07:51:05 -0300

    Fix

--- src/bin/lightning_lendingd.rs
+++ src/bin/lightning_lendingd.rs
@@ -1,13 +1,30 @@
-use crate::lightning_lending;
+extern crate lightning;
+extern crate dlc;
+extern crate nostr;
 
 use lightning::{ln::peer_channel::ChannelDetails, util::logger::Logger};
 use dlc::{contract::Contract, payout_curve::PayoutCurve};
 use nostr::{self, NodeClient};
 
-
-use lightning_lending::{
-  bitcoin::Bitcoin,
-  nostr::Nostr,
-  dlc::Dlc,
-  lightning::Lightning,
+use crate::lightning_lending::{
+    bitcoin::Bitcoin,
+    nostr::Nostr,
+    dlc::Dlc,
+    lightning::Lightning,
 };
+
+fn main() {
+
+  let logger = Logger::new();
+
+  // Assuming these `new` methods exist and accept a logger
+  let lightning = Lightning::new(&logger);
+  let bitcoin = Bitcoin::new(&logger);
+  let dlc = Dlc::new(&logger);
+  let nostr = Nostr::new(&logger);
+  let contract = Contract::new(&logger);
+  let payout_curve = PayoutCurve::new(&logger);
+  let node_client = NodeClient::new(&logger);
+
+}
+

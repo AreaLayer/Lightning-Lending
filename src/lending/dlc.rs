2ca8c7c258685db6d17e0d8e412bc1c322a4013d
223880 <rafael.segala45@gmail.com>
2024-08-26 07:07:25 -0300

    Fix dlcs

--- src/lending/dlc.rs
+++ src/lending/dlc.rs
@@ -11,7 +11,9 @@ let oracle_info = OracleInfo {
     maturity_time: 1651339200, // Timestamp for 12:00 UTC on April 30, 2023
     num_ticks: 1000, // Number of price ticks
     tick_size: 1000, // Size of each price tick (in sats)
-};let contract_info = ContractInfo {
+};
+
+let contract_info = ContractInfo {
     local_amount: 1000000, // Amount of the loan (in sats)
     oracle_r_value: Some(oracle_r_value), // Oracle's R value (if already known)
     local_collateral: 2000000, // Collateral amount (in sats)
@@ -19,20 +21,22 @@ let oracle_info = OracleInfo {
     refund_lock_time: 1649790400, // Timestamp for May 12, 2023 (when the refund can be claimed)
     oracle_pub_key: oracle_pub_key, // Oracle's public key
 };
+
 let dlc_manager = DlcManager::new();
+
 let dlc = BtcDlc::new(
     contract_info,
     oracle_info,
     dlc_manager.get_random_bytes(32),
     Some(dlc_manager.get_random_bytes(32)),
 );
+
 let (funding_tx, funding_address, funding_amount) = get_funding_info(); // Get the funding information for the Lightning channel
 let mut psbt_map = HashMap::new();
 let psbt = get_signed_psbt(); // Get a signed PSBT for the funding transaction
 psbt_map.insert(funding_address, psbt);
-let fund_result = dlc.fund_contract(psbt_map,funding_address, psbt);
+let fund_result = dlc.fund_contract(psbt_map, funding_address, psbt);
 
-};
-/ Define the network you want to use, e.g. testnet or mainnet
+// Define the network you want to use, e.g., testnet or mainnet
 let network = bitcoin::network::constants::Network::Testnet;
 let network = bitcoin::network::constants::Network::Signet;

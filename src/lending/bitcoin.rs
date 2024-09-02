9f3f1bf20d6a1ad8e3131f4bb3da70e562ab177e
223880 <rafael.segala45@gmail.com>
2024-09-02 12:31:30 -0300

    Add Taproot support

--- src/lending/bitcoin.rs
+++ src/lending/bitcoin.rs
@@ -1,6 +1,7 @@
 use bitcoin::secp256k1::{PublicKey, SecretKey};
 use bitcoin::{Address, Network};
 use miniscript::bitcoin::PublicKey as MiniscriptPublicKey;
+use bitcoin::taproot::{TapTweak, TapTweakExt};
 
 pub fn get_address(secret_key: &SecretKey, network: Network) -> Address {
     let public_key = PublicKey::from_secret_key(secret_key);
@@ -17,3 +18,10 @@ pub fn get_miniscript_address_from_string(address: &str, network: Network) -> Mi
     let public_key = PublicKey::from_str(address).unwrap();
     MiniscriptPublicKey::from_public_key(&public_key);
 }
+pub fn get_taproot_address(secret_key: &SecretKey, network: Network) -> Address {
+    let public_key = PublicKey::from_secret_key(secret_key);
+    let taproot_tweak = TapTweak::from_secret_key(secret_key);
+    let taproot_public_key = public_key.taproot_tweak(&taproot_tweak);
+    Address::p2tr(&taproot_public_key, network)
+}
+
